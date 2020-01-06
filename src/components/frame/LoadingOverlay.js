28c49c5465602fba037face812884d6cc30f9b73
nico.funke <nico.funke@stud.uni-goettingen.de>
2020-01-06 18:50:21 +0100

    Logo designs

--- src/components/frame/LoadingOverlay.js
+++ src/components/frame/LoadingOverlay.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MDBProgress } from 'mdbreact'
+import Logo from '../../assets/logo_font.png'
 
 export default class LoadingOverlay extends React.Component {
 
@@ -13,8 +13,12 @@ export default class LoadingOverlay extends React.Component {
             <div className="position-fixed h-100 w-100 overflow-hidden bg-white loading-overlay">
                 <div className="row h-100">
                     <div className="col-12 my-auto row">
-                        <div className="text-center col-12 mb-2">{this.props.message}</div>
-                        <div className="col-sm-6 offset-sm-3"><MDBProgress value={100 * this.props.loadingProgress} /></div>
+                        <div className="text-center col-12 mb-4"> <img src={Logo} alt="Logo" height="80px" /></div>
+                        <div className="col-sm-4 offset-sm-4">
+                            <div class="progress">
+                                <div class="progress-bar blue-gradient" role="progressbar" style={{ width: `${100 * this.props.loadingProgress}%` }}></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
