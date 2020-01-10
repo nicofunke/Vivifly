3cab1d38bc7209a2a8c38708ed7efb50ab86f324
nico.funke <nico.funke@stud.uni-goettingen.de>
2020-01-10 17:42:42 +0100

    New Starting screen

--- src/components/frame/StartingOverlay.js
+++ src/components/frame/StartingOverlay.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import Logo from '../../assets/logo_font.png'
+import LogoGrey from '../../assets/logo_font_grey.png'
 import { AppContext } from '../Application/AppContext'
 
 export default class StartingOverlay extends React.Component {
 
     render() {
         const loadingProgress = this.context.applicationState.unityLoadingProgress
-        if(loadingProgress >= 1.0){
+        if (loadingProgress >= 1.0) {
             return null
         }
         return <>
             <div className="position-fixed h-100 w-100 overflow-hidden bg-white starting-overlay">
                 <div className="row h-100">
                     <div className="col-12 my-auto row">
-                        <div className="text-center col-12 mb-4"> <img src={Logo} alt="Logo" height="80px" /></div>
-                        <div className="col-sm-4 offset-sm-4">
-                            <div className="progress">
-                                <div className="progress-bar blue-gradient" role="progressbar" style={{ width: `${100 * loadingProgress}%` }}></div>
+                        <div className="text-center col-12 mb-4">
+                            <div className="position-relative d-inline-block" style={{ maxWidth: "230px" }}>
+                                <div className="position-absolute h-100 overflow-hidden" style={{ width: `${100 * loadingProgress}%` }}>
+                                    <img src={Logo} alt="Logo" height="80px" />
+                                </div>
+                                <img src={LogoGrey} alt="Logo" height="80px" />
                             </div>
                         </div>
                     </div>
